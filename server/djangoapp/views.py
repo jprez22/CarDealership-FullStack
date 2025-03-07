20e312378bcfa40968db489c016ed82e807f3f5a
jprez22 <jpreziuso14@gmail.com>
2025-03-07 11:57:46 -0600

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -75,7 +75,8 @@ def registration(request):
         # Create user in auth_user table
         user = User.objects.create_user(username=username,
                     first_name=first_name,
-                            last_name=last_name, password=password, email=email)
+                            last_name=last_name,
+                            password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
