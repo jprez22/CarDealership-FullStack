78afa71f558b4d5113999c697f3c12b39bf5066f
jprez22 <jpreziuso14@gmail.com>
2025-03-07 10:37:49 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,9 @@
+# from django.utils.timezone import now
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+# Create your models here.                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -15,7 +15,8 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -30,7 +31,7 @@ class CarModel(models.Model):
         ('WHITE', 'White'),
         ('RED', 'Red'),
         ('BLUE', 'Blue'),
-    ]    
+    ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
         validators=[
